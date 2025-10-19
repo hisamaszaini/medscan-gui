0bfdaac1e65679b6a3a8ee7e00c6fcc0f7b264cb
hisamaszaini <muhammadhisamaszaini@gmail.com>
2025-10-19 20:00:28 +0700

    Fourth

--- pages/image_capture_page.py
+++ pages/image_capture_page.py
@@ -166,14 +166,14 @@ class ImageCapturePage(QWidget):
                 self.picam = Picamera2(camera_num=camera_num)
                 config = self.picam.create_preview_configuration(main={"size": (640, 480)})
                 self.picam.configure(config)
+                self.picam.start()
                 self.picam.set_controls({
                     "AwbEnable": True,
         			"AwbMode": "auto",
         			"AeEnable": True,
         			"AnalogueGain": 1.0,
-                    "ExposureTime": 20000
+ 			       "ExposureTime": 20000
     			})
-                self.picam.start()
                 self.capture_button.setEnabled(True)
                 self.timer.timeout.connect(self.update_frame_picam)
                 self.timer.start(30)
