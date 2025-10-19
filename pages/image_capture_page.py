e2e7ad27522f959ee7092984ea22a67720138599
hisamaszaini <muhammadhisamaszaini@gmail.com>
2025-10-19 20:08:14 +0700

    Fourth

--- pages/image_capture_page.py
+++ pages/image_capture_page.py
@@ -167,15 +167,6 @@ class ImageCapturePage(QWidget):
                 config = self.picam.create_preview_configuration(main={"size": (640, 480)})
                 self.picam.configure(config)
                 self.picam.start()
-                try:
-                     self.picam.set_controls({
-                    	"AwbEnable": True,
-                    	"AwbMode": "auto",
-                    	"AeEnable": True
-                	})
-                except Exception as e:
-                    print(f"Kontrol tambahan gagal, tetap aman: {e}")
-
                 self.capture_button.setEnabled(True)
                 self.timer.timeout.connect(self.update_frame_picam)
                 self.timer.start(30)
